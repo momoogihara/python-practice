84dc9fdf4c5d2df9c41b82fd120c650d3102e196
momoogihara <pirouettmomo@gmail.com>
2026-04-27 19:25:14 +0900

    Day3

---
+++ test_if-for.py
@@ -0,0 +1,30 @@
+name="shogo"
+
+if name =="momo" :
+ print("OK")
+else:
+ print("NO")
+
+ for i in range(1,6):
+  print(i)
+
+  for i in range(3):
+    print("hello")
+
+for i in range(1,6):
+ if i % 2 ==0:
+  print (i,"even")
+ else:
+  print(i,"odd")
+
+x = 1
+
+if x == 1:
+    print("A")
+    print("B")
+
+print("C")
+
+for i in range(3):
+  if i==1:
+   print("hit")
---
+++ test_name-age.py
@@ -0,0 +1,12 @@
+name="momo"
+age="29"
+
+print(name)
+print(age)
+
+x = 10
+y = 3
+
+print(x+y)
+print(x*y)
+
